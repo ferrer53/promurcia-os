7e4d1d385a39160d35436e276721918838e2a196
Tamgama <tamara.garmart@gmail.com>
2026-07-29 17:37:04 +0200

    fix: configure SSL for Render PostgreSQL connections

--- db/connection.ts
+++ db/connection.ts
@@ -3,6 +3,11 @@ import { Client } from "pg";
 import * as schema from "./schema";
 import * as relations from "./relations";
 
-const client = new Client({ connectionString: process.env.DATABASE_URL });
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 await client.connect();
 export const db = drizzle(client, { schema: { ...schema, ...relations } });
